71fb05f30747eecdee04d96c61a20ee88437d1e3
Ivan Vodopianov <vis3010@gmail.com>
2022-09-12 01:54:35 +0300

    start task-3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,8 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import 'notiflix/dist/notiflix-3.2.5.min.css';
+
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
